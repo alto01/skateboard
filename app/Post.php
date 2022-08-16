20651f1da333a8fcafa60618aab675aafe35d715
alto01 <haratakumin@gmail.com>
2022-08-16 14:43:05 +0900

    一覧表示、詳細表示作成

---
+++ app/Post.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+  public function getPaginateByLimit(int $limit_count = 10)
+  {
+        // updated_atで降順に並べたあと、limitで件数制限をかける
+    return $this::with('user')->orderBy('updated_at', 'DESC')->paginate($limit_count);
+  }
+  public function user()
+  {
+    return $this->belongsTo('App\User');
+  }
+}
