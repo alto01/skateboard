0f1c0ce7ee69c76d1f33e4dd96c021aeb63c8724
alto01 <haratakumin@gmail.com>
2022-10-01 13:56:57 +0900

    調整

--- app/Post.php
+++ app/Post.php
@@ -15,6 +15,11 @@ class Post extends Model
     'image',
     ];
     
+  protected $appends = [
+      'likes_count', 'liked_by_user',
+    ];
+
+    
   public function getPaginateByLimit(int $limit_count = 10)
   {
         // updated_atで降順に並べたあと、limitで件数制限をかける
@@ -24,29 +29,32 @@ class Post extends Model
   {
     return $this->belongsTo('App\User');
   }
-  public function likes()
-  {
-    return $this->hasMany('App\Like');
-  }
+   public function likes()
+   {
+     return $this->hasMany('App\Like');
+   }
   
-  /**
-  * リプライにLIKEを付いているかの判定
-  *
-  * @return bool true:Likeがついてる false:Likeがついてない
-  */
-  public function is_liked_by_auth_user()
-  {
-    $id = Auth::id();
+   /**
+   * リプライにLIKEを付いているかの判定
+   *
+   * @return bool true:Likeがついてる false:Likeがついてない
+   */
+   public function is_liked_by_auth_user()
+   {
+     $id = Auth::id();
 
-    $likers = array();
-    foreach($this->likes as $like) {
-      array_push($likers, $like->user_id);
-    }
+     $likers = array();
+     foreach($this->likes as $like) {
+       array_push($likers, $like->user_id);
+     }
 
-    if (in_array($id, $likers)) {
-      return true;
-    } else {
-      return false;
-    }
-  }
+     if (in_array($id, $likers)) {
+       return true;
+     } else {
+       return false;
+     }
+   }
+  
+  
+  
 }
